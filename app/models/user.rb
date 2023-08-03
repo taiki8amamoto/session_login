5b204fc7a0d3009f40debe5500e4aaddf56634e7
taiki yamamoto <ultra0115.20@gmail.com>
2023-08-03 16:36:47 +0900

    アソシエーション2の途中

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :blogs
   validates :name, presence: true, length: { maximum: 30 }
   validates :email, presence: true, length: { maximum: 225 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i } ,uniqueness: true
   before_validation { email.downcase! }
